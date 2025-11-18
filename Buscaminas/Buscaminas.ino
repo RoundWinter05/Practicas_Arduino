14fc2158e110307010a8d5b290040e93b409ab2c
RoundWinter05 <pablohdez025@gmail.com>
2025-11-18 15:38:30 -0600

    Seed de buscaminas corregido y comentado

--- Buscaminas/Buscaminas.ino
+++ Buscaminas/Buscaminas.ino
@@ -1,6 +1,8 @@
-int ejemplo[6];
+//array to store the location of the "mine"
+int mineLocation[6];
 int randomNumber;
 
+//Pin declaration
 int Led1 = 5;
 int Led2 = 6;
 int Led3 = 7;
@@ -10,24 +12,18 @@ int Led6 = 10;
 
 int interruptionPin = 2;
 
-int pressedButton = -1;
+int pressedButton = -1; // <- -1 = none
 
 volatile bool resetFlag = false;
 
-int tries = 3;
+int attempts = 3; // amount of tries before reseting the game
 
 void setup() {
   Serial.begin(9600);
   pinMode(interruptionPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(interruptionPin), resetButton, FALLING);
-  /*
-  pinMode(button0, INPUT_PULLUP);
-  pinMode(button1, INPUT_PULLUP);
-  pinMode(button2, INPUT_PULLUP);
-  pinMode(button3, INPUT_PULLUP);
-  pinMode(button4, INPUT_PULLUP);
-  pinMode(button5, INPUT_PULLUP);
-*/
+
+  //Set "analog in" pins to read the buttons
   pinMode(A0, INPUT_PULLUP);
   pinMode(A1, INPUT_PULLUP);
   pinMode(A2, INPUT_PULLUP);
@@ -35,6 +31,7 @@ void setup() {
   pinMode(A4, INPUT_PULLUP);
   pinMode(A5, INPUT_PULLUP);
 
+  //Set the "digital" pins to send the output of each led
   pinMode(Led1, OUTPUT);
   pinMode(Led2, OUTPUT);
   pinMode(Led3, OUTPUT);
@@ -42,16 +39,20 @@ void setup() {
   pinMode(Led5, OUTPUT);
   pinMode(Led6, OUTPUT);
 
+  // Begin the first round of the game
   reset();
 }
 
 void loop() {
+  //check if the interruption button was pressed
+  //this will set the flag to true ^^^^^^^^^^^^
   if (resetFlag == true) {
-    reset();
+    reset(); // <- And obviously rest the game
   }
 
-  if (tries > 0) {
-    checkAttempt();
+  //Verify if you still have remaining attempts
+  if (attempts > 0) {
+    checkAttempts();
   } else {
     Serial.println("Se acabaron los intentos, Intente de nuevo");
     Serial.println();
@@ -59,8 +60,8 @@ void loop() {
   }
 }
 
+//Method to read the buttons and set pressed button variable :O
 int readButton() {
-
   int buttonState0 = digitalRead(A5);
   int buttonState1 = digitalRead(A4);
   int buttonState2 = digitalRead(A3);
@@ -92,94 +93,97 @@ int readButton() {
 }
 
 void checkAttempt() {
+  // Check the button pressed and set it inside the switch 
+  // These prints are merely for debugging and checking if the game worked before the leds 
+
   switch (readButton()) {
     case 0:
-      Serial.println("El boton 0 fue presionado");
-      if (ejemplo[pressedButton] != 0) {
-        Serial.println("Felicidades");
+      Serial.println("El boton 0 fue presionado"); 
+      if (mineLocation[pressedButton] != 0) {
+        Serial.println("Felicidades"); 
         successSecuence();
         reset();
       } else {
         digitalWrite(Led1, HIGH);
         Serial.println("No está aquí  :(");
-        tries -= 1;
+        attempts -= 1;
         Serial.print("Intentos: ");
-        Serial.println(tries);
+        Serial.println(attempts);
         Serial.println();
       }
       break;
     case 1:
       Serial.println("El boton 1 fue presionado");
-      if (ejemplo[pressedButton] != 0) {
+      if (mineLocation[pressedButton] != 0) {
         Serial.println("Felicidades");
         successSecuence();
         reset();
       } else {
         digitalWrite(Led2, HIGH);
         Serial.println("No está aquí  :(");
-        tries -= 1;
+        attempts -= 1;
         Serial.print("Intentos: ");
-        Serial.println(tries);
+        Serial.println(attempts);
         Serial.println();
       }
       break;
     case 2:
       Serial.println("El boton 2 fue presionado");
-      if (ejemplo[pressedButton] != 0) {
+      if (mineLocation[pressedButton] != 0) {
         Serial.println("Felicidades");
         successSecuence();
         reset();
       } else {
         digitalWrite(Led3, HIGH);
         Serial.println("No está aquí  :(");
-        tries -= 1;
+        attempts -= 1;
         Serial.print("Intentos: ");
-        Serial.println(tries);
+        Serial.println(attempts);
         Serial.println();
       }
       break;
     case 3:
       Serial.println("El boton 3 fue presionado");
-      if (ejemplo[pressedButton] != 0) {
+      if (mineLocation[pressedButton] != 0) {
         Serial.println("Felicidades");
         successSecuence();
         reset();
       } else {
         digitalWrite(Led4, HIGH);
         Serial.println("No está aquí  :(");
-        tries -= 1;
-        Serial.print("Intenos: ");
-        Serial.println(tries);
+        attempts -= 1;
+        Serial.print("Intentos: ");
+        Serial.println(attempts);
         Serial.println();
       }
       break;
     case 4:
       Serial.println("El boton 4 fue presionado");
-      if (ejemplo[pressedButton] != 0) {
+      if (mineLocation[pressedButton] != 0) {
         Serial.println("Felicidades");
         successSecuence();
         reset();
       } else {
         digitalWrite(Led5, HIGH);
         Serial.println("No está aquí  :(");
-        tries -= 1;
+        attempts -= 1;
         Serial.print("Intentos: ");
-        Serial.println(tries);
+        Serial.println(attempts);
         Serial.println();
       }
       break;
     case 5:
       Serial.println("El boton 5 fue presionado");
-      if (ejemplo[pressedButton] != 0) {
+      if (mineLocation[pressedButton] != 0) {
         Serial.println("Felicidades");
         successSecuence();
         reset();
       } else {
         digitalWrite(Led6, HIGH);
         Serial.println("No está aquí  :(");
-        tries -= 1;
+        attempts -= 1;
         Serial.print("Intentos: ");
-        Serial.println(tries);
+        Serial.println(attempts);
         Serial.println();
       }
       break;
@@ -187,7 +191,10 @@ void checkAttempt() {
   pressedButton = -1;
 }
 
+// Either after winning or failing the reset secuence must play 
+// All roads lead to the resetSecuence
 void resetSecuence() {
+  //Small sweep to clear the ""board""
   digitalWrite(Led1, HIGH);
   delay(100);
   digitalWrite(Led1, LOW);
@@ -214,6 +221,7 @@ void resetSecuence() {
 }
 
 void successSecuence() {
+  //Turn off the leds and...
   digitalWrite(Led1, LOW);
   digitalWrite(Led2, LOW);
   digitalWrite(Led3, LOW);
@@ -221,6 +229,7 @@ void successSecuence() {
   digitalWrite(Led5, LOW);
   digitalWrite(Led6, LOW);
 
+  //Small led alternating dance :D
   for (int i = 0; i < 3; i++) {
     digitalWrite(Led1, HIGH);
     digitalWrite(Led3, HIGH);
@@ -249,33 +258,37 @@ void resetButton() {
 }
 
 void reset() {
-  for (int i = 0; i < 5; i++) {
-    ejemplo[i] = 0;
+  //clear all values inside the array
+  for (int i = 0; i < 6; i++) {
+    mineLocation[i] = 0;
   }
-  randomNumber = random(0, 5);
 
-  ejemplo[randomNumber] = 1;
+  //Set a new random seed to generate a new spot for the mine
+  randomSeed(micros());
+  randomNumber = random(0, 6);
 
-  Serial.println("Se ha reiniciado el juego");
-  Serial.println(randomNumber);
+  //""Hide the mine"" on the newly generated position
+  mineLocation[randomNumber] = 1;
+
+  //Reset the amount of attempts remaining
+  attempts = 3;
+
+  //Indicate the user the game has been reseted
+  Serial.println("Se ha reiniciado el juego");  
+
+  //Show the array where the mine is located
+  //This is mainly for debugging and kinda breaks the emotion of the game
+  //So this can be commented to keep it secret.
   Serial.println("Array con el numero aleatorio:");
   for (int i = 0; i < 6; i++) {
-    Serial.print(ejemplo[i]);
+    Serial.print(mineLocation[i]);
     Serial.print(",  ");
   }
   Serial.println();
   Serial.println();
 
-  tries = 3;
-  /*
-  digitalWrite(Led1, LOW);
-  digitalWrite(Led2, LOW);
-  digitalWrite(Led3, LOW);
-  digitalWrite(Led4, LOW);
-  digitalWrite(Led5, LOW);
-  digitalWrite(Led6, LOW);
-*/
+
+
   resetSecuence();
-  delay(1000);
   resetFlag = false;
 }
